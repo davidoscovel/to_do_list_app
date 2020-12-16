df5d7dca1d56faf23ae45b76fe5cc6a722ef5e8c
David Scovel <davidoscovel@gmail.com>
2020-12-15 17:27:22 -0700

    Project Components and Title Banner

---
+++ app/javascript/components/Home/Home.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import TitleBanner from "./TitleBanner";
+
+class Home extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            to_dos: [
+                {
+                    id: 1,
+                    title: 'Laundry',
+                    description: 'Wash the whites.',
+                    deadline: new Date(2020, 12, 23, 12),
+                    status: "Pending",
+                    active: false
+                },
+                {
+                    id: 1,
+                    title: 'Build App',
+                    description: 'Build To-Do-List app.',
+                    deadline: new Date(2020, 12, 20, 24),
+                    status: "Pending",
+                    active: true
+                },
+                {
+                    id: 1,
+                    title: 'Write Speech',
+                    description: 'Wash the whites.',
+                    deadline: new Date(2020, 12, 23, 12),
+                    status: "Complete",
+                    active: false
+                },
+            ]
+        }
+    }
+    render() {
+        return(
+            <div>
+                <TitleBanner/>
+            </div>
+        )
+    }
+}
+
+export default Home
---
+++ app/javascript/components/Home/TitleBanner.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Section = styled.section`
+    background-color: #229cfe;
+    padding: 30px;
+    color: #fff;
+`
+
+const Title = styled.h1`
+    color: #fff;
+    font-weight: 700;
+    font-size: 50px;
+    text-align: center;
+`
+
+const SubTitle = styled.p`
+    color: #fff;
+    font-weight: 400;
+    font-size: 20px;
+    text-align: center;
+`
+
+const TitleBanner = () => {
+    return(
+        <Section>
+        <div>
+        <Title>TO-DO-LIST</Title>
+        <SubTitle>A Ruby on Rails Application with React</SubTitle>
+        </div>
+        </Section>
+    )
+}
+
+export default TitleBanner
